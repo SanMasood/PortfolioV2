628fce4ea56ebc51c13b173251e4c13439a0f0aa
SanMasood <sanamasood.giki@gmail.com>
2020-06-22 15:42:53 +0100

    fixed menu toggler issue not closing

--- main.js
+++ main.js
@@ -2,15 +2,17 @@ $(document).ready(function () {
     $('.menu-toggler').on('click', function () {
         $(this).toggleClass('open');
         $('.top-nav').toggleClass('open');
+
     });
     $('.top-nav .nav-link').on('click', function () {
-        $(menu-toggler).removeClass('open');
+        $('.menu-toggler').removeClass('open');
         $('.top-nav').removeClass('open');
     });
     $('nav a[href*="#"]').on('click', function () {
         $('html, body').animate({
             scrollTop: $($(this).attr('href')).offset().top - 100
         }, 2000);
+
     });
     $('#up').on('click', function () {
         $('html, body').animate({
@@ -19,7 +21,8 @@ $(document).ready(function () {
     })
    AOS.init({
        easing: 'ease',
-       duration: 1800
+       duration: 1800,
+       //once:true
    });
 
        
